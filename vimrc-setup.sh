b2a20ea70f940e4a03bf1589aef425c65ab88115
Justin Oeder <jcoeder@gmail.com>
2022-01-02 00:30:34 -0500

    Update vimrc-setup.sh

--- vimrc-setup.sh
+++ vimrc-setup.sh
@@ -3,15 +3,24 @@
 if [ -f /etc/lsb-release ]; then
     # For some versions of Debian/Ubuntu without lsb_release command
     . /etc/lsb-release
-    apt-get install vim curl -y
+    apt-get install vim curl git make -y
 elif [ -f /etc/debian_version ]; then
     # Older Debian/Ubuntu/etc.
-    apt-get install vim curl -y
+    apt-get install vim curl git make -y
 elif [ -f /etc/redhat-release ]; then
     # Older Red Hat, CentOS, etc.
-    apt-get install vim curl -y
+    apt-get install vim curl git make -y
 fi
 
+git clone https://github.com/vim/vim.git
+cd vim
+git pull
+./configure --with-features=huge
+make
+make check
+make install
+make clean
+make distclean
 
 mkdir -p ~/.vim/plugged/
 mkdir -p ~/.vim/undodir/
