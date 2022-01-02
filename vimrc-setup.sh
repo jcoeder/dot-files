842dd0caf03b6c1a639d381bb79dedb63effd1e0
Justin Oeder <jcoeder@gmail.com>
2022-01-02 02:59:08 -0500

    Update vimrc-setup.sh

--- vimrc-setup.sh
+++ vimrc-setup.sh
@@ -19,9 +19,9 @@ make distclean
 ./configure --with-features=huge \
     --enable-multibyte \
     --enable-pythoninterp=yes \
-    --with-python-config-dir=/usr/lib/python2*/config* \
+    --with-python-config-dir=/usr/lib/python2*/config-* \
     --enable-python3interp=yes \
-    --with-python3-config-dir=/usr/lib/python3.*/config* \
+    --with-python3-config-dir=/usr/lib/python3.*/config-* \
     --enable-cscope \
     --prefix=/usr/local/
 
