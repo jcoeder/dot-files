a82d0368d20687ec92f97f3e8bbd8c2f9abdedde
Justin Oeder <jcoeder@gmail.com>
2022-01-02 00:20:49 -0500

    Update vimrc-setup.sh

--- vimrc-setup.sh
+++ vimrc-setup.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-mkdir -p ~/.vim/plugged/
-mkdir -p ~/.vim/undodir/
+if [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    sudo apt-get install vim curl -y
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    sudo apt-get install vim curl -y
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    sudo apt-get install vim curl -y
+fi
 
-curl https://raw.githubusercontent.com/jcoeder/dot-files/master/.vimrc \
+
+sudo mkdir -p ~/.vim/plugged/
+sudo mkdir -p ~/.vim/undodir/
+
+sudo curl https://raw.githubusercontent.com/jcoeder/dot-files/master/.vimrc \
     --output ~/.vimrc
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PlugInstall +qall
